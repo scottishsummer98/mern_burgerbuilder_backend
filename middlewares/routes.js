76a6eb5cc551780ea2e51a006d1f4e1b3b7a0286
scottishsummer98 <samiremail98@gmail.com>
2023-11-11 19:14:00 +0600

    Payment route added to middleware

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -1,9 +1,11 @@
 const userRouter = require("../routers/userRouter");
 const orderRouter = require("../routers/orderRouter");
 const profileRouter = require("../routers/profileRouter");
+const paymentRouter = require("../routers/paymentRouter");
 
 module.exports = (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/order", orderRouter);
   app.use("/api/profile", profileRouter);
+  app.use("/api/payment", paymentRouter);
 };
